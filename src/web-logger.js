42a63651e1b5c1bdd6b7d6620efdcb9b35398b53
zengzhifei <zengzhifei@outlook.com>
2018-09-11 18:25:33 +0800

    提交测试文件

--- src/web-logger.js
+++ src/web-logger.js
@@ -1,7 +1,19 @@
+//utils
+import Utils from './utils.js';
+//logger network
 import WebNetwork from './web-network.js';
 
 class WebLogger {
-    constructor() {
+    constructor(options) {
+        this.configs = {
+            monitorModules: ['network']
+        };
+        if (options.hasOwnProperty('monitorModules') && Utils.isArray(options.monitorModules) && options.monitorModules.length) {
+            let monitorModules = [];
+            for (let i in options.monitorModules) {
+                this.configs.monitorModules.indexOf(options.monitorModules[i]) > -1 && monitorModules.push(options.monitorModules[i]);
+            }
+        }
         new WebNetwork();
     }
 
